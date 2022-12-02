4019a62363abf7ad29b636b406a58e46d5dd480f
jokkekoo <jokke.kivijarvi@gmail.com>
2022-12-02 11:06:10 +0200

    new web techstack

--- UWB/serial.py
+++
@@ -1,4 +0,0 @@
-import serial
-
-for x in range(2):
-    print("asd")
--- drone/request.py
+++ drone/request.py
@@ -1,7 +1,7 @@
 import requests
 
 t = {'c':'takeoff'}
-# r = requests.post('http://192.168.191.214:9000/',payload=t)
-r = requests.post('http://0.0.0.0:8000/',json=t)
+# r = requests.post('http://193.167.101.41:8080/',payload=t)
+r = requests.post('http://193.167.101.41:8080/',json=t)
 
 print(r.url)
