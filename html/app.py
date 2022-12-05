3acf98cb6da505b5be76539faa99403f16a663dc
jokkekoo <jokke.kivijarvi@gmail.com>
2022-12-05 15:14:14 +0200

    lisäyksiä

--- html/app.py
+++ html/app.py
@@ -21,6 +21,19 @@ def script():
     requests.post('http://193.167.101.43:8080/', json=payload)
     return render_template("index.html")
 
+@app.route("/emergency", methods=["GET","POST"])
+def emergency():
+    payload = {'c':'emergency'}
+    requests.post('http://193.167.101.43:8080/', json=payload)
+    return render_template("index.html")
+
 @app.route("/about")
 def about():
     return render_template("projektista.html")
+
+# Palvelimen käynnistys :
+# sudo systemctl restart nginx , jos muokannut nginx asetuksia
+# html kansion sisällä sudo gunicorn --workers 5 wsgi:app
+# screen päälle ja Ctrl+A+C
+# sitten drone kansioon ja python tello.py 8080 
+#
